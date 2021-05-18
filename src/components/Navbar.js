b3cab6d5a0e8e937b43823122a7a8e31d7801e5a
Landon Tomaine <latomaine@live.com>
2021-05-18 15:29:57 -0400

    Finish navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from "react";
 import logo from "../logo4.png";
+// REACT FONTAWESOME IMPORTS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
@@ -10,7 +13,7 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                 aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <FontAwesomeIcon icon={faBars} style={{ color: "#000"}}/>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -19,7 +22,16 @@ const Navbar = () => {
                         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="#">About Me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Skills and Tools</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Work History</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Hobbies</a>
                     </li>
 
                     </ul>
