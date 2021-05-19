ba716924c6070702bf10f075297175dd76be532c
Landon Tomaine <latomaine@live.com>
2021-05-18 20:19:04 -0400

    Add more text to aboutme

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,8 +13,12 @@ const AboutMe = () => {
                 <div className="col-lg-6 col-xm-12">
                     <h1 className="about-heading">About Me</h1>
                     <p>
-                        Hello! My name is Landon. I am a Software Engineer with a focus on ditributed enterprise software.
-                    </p>
+                        Hello! My name is Landon. I am a results-oritented and innovative Software Engineer with over 2 years of professional experience.
+                        I can easily communicate complex technical requirements to non-technical stakeholders and have an excellent leadership record of
+                        leading development teams in enterprise-wide development projects. I am experienced with high-volume transaction processing with
+                        distributed applications.</p>
+
+                        <p>My main focus is in the .NET stack with high proficiency in C# and T-SQL.</p>
                 </div>
             </div>
         </div>
